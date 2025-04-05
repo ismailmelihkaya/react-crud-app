0784aa82125c848392113c667381bffc8d7673e9
İsmail Kaya <ismailkyaa65@gmail.com>
2025-04-05 12:53:31 +0300

    product migration, model, ReContr created and db migrated

---
+++ app/Models/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    //
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'featured_image',
+        'featured_image_original_name',
+    ];
+
+}
